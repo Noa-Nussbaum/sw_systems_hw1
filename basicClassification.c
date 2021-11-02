487cc2942d1a1a4fcf3d95701d217b06ce79799d
Noa <76524924+Noa-Nussbaum@users.noreply.github.com>
2021-11-02 21:01:40 +0200

    One is a prime number

--- basicClassification.c
+++ basicClassification.c
@@ -1,5 +1,7 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <math.h>
+#include "NumClass.h"
 
 int isPrime(int n){
     if(n==1){
@@ -33,3 +35,41 @@ int isStrong(int n){
     }
     return n==ans;
 }
+||||||| ea5a62d
+=======
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n){
+    if(n==1){
+        return 1;
+    }
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int Factorial(int n){
+    int ans = 1;
+    while(n!=1){
+        ans = ans*n;
+        n = n-1;
+    }
+    return ans;
+}
+
+int isStrong(int n){
+    int ans = 0; 
+    int x=0;
+    int y =n;
+    while(y>0){
+        x = y%10;
+        ans = ans + Factorial(x);
+        y = (y - x)/10;
+    }
+    return n==ans;
+}
+>>>>>>> 225e88c3a0a4fe6d1f0f632277b6ea58819872df
