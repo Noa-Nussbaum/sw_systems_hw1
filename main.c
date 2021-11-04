3165bfe01e6241b8e5d6a36cf70c345b4cbcf42a
Noa <76524924+Noa-Nussbaum@users.noreply.github.com>
2021-11-04 15:27:46 +0200

    Final tweaks

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(){
         min = a;
     }
   
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i = min; i<=max; i++){
         if(isArmstrong(i)){
             printf(" %d",i );
@@ -29,7 +29,7 @@ int main(){
     }
     printf("\n");
 
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for(int i = min; i<=max; i++){
         if(isPalindrome(i)){
             printf(" %d",i );
@@ -37,20 +37,19 @@ int main(){
     }
     printf("\n");
 
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i = min; i<=max; i++){
         if(isPrime(i)){
             printf(" %d",i );
         }
     }
     printf("\n");
-    printf("The Strong numbers are: ");
-
+    printf("The Strong numbers are:");
     for(int i = min; i<=max; i++){
         if(isStrong(i)){
             printf(" %d",i );
         }
     }
     printf("\n");
-   
+    
 }
